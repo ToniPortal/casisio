867d0c674f361cada1fe51fb5b14f3391e5fdbc2
ToniPortal <tonitoctoc13@gmail.com>
2022-09-29 13:53:59 +0200

    hop hop

--- src/js/erreurpage.js
+++ src/js/erreurpage.js
@@ -3,14 +3,6 @@
 window.onload = function() {
     const back = document.getElementById('homePage');
     
-    (document.getElementById("homePage")).addEventListener('click', function(e) {
-e.preventDefault();
-        menu()
-  
-    });
-
-
-
     (document.getElementById("avant")).addEventListener('click', function(e) {
 e.preventDefault();
         gobak()
@@ -20,9 +12,8 @@ e.preventDefault();
 
 
 
-function menu() {
-document.location.href= "/"
-}
+
+
 function gobak() {
     history.back()
 }
---
+++ src/js/login.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    (document.getElementById("createbtn")).addEventListener('click', function(e) {
+        e.preventDefault();
+        document.location.href = "/create"
+          
+            });
+
+}
